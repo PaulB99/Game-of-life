33ee37a71f29163ce180a4180e57831b4f65e798
Paul <paulbyrne99@rocketmail.com>
2020-06-09 12:10:39 +0100

    displays nicely

--- Julia/life.jl
+++ Julia/life.jl
@@ -6,13 +6,27 @@ blinker = [0 0 0 0 0;
            0 0 0 0 0;
            0 0 0 0 0]
 
+function showline(x)
+    for i in x
+        if i == 1
+            print("# ")
+        else
+            print(". ")
+        end
+    end
+    print("\n")
+end
 
 # A recursive function that runs the game of life. Args of grid, steps
 function life(grid, steps)
     if steps <= 0
         return
     end
+
     newgrid = grid
-    println(newgrid)
+    for r in eachrow(newgrid)
+        showline(r)
+    end
+    println("\n")
     life(newgrid, steps-1)
 end
