82b60ca3dee5c7e9b37c6ab9f8bda83e114d281a
Paul <paulbyrne99@rocketmail.com>
2020-06-12 14:56:41 +0100

    adding helper functions

--- Julia/life.jl
+++ Julia/life.jl
@@ -17,18 +17,31 @@ function showline(x)
     print("\n")
 end
 
+# A function to count the live neighbours of a cell
+function countneighbours((x, y), grid)
+    n = 0
+    for i in -1:1
+        for j in -1:1
+            if i == 0 && j == 0 # Ignore central cell
+            elseif x + i < 0 || y + j < 0 # goes out of bounds
+            elseif x + i > eachrow(grid) || y + j > eachcol(grid)
+            else n += 1
+            end
+        end
+    end
+    return n
+end
+
 # A recursive function that runs the game of life. Args of grid, steps
 function life(grid, steps)
     if steps <= 0  # Break if it reaches the end
         return
     end
-    for x in grid
-        liveneighbours = 0  # Find live neighbours
-        for i in -1:1
-            for j in -1:1
-                position[x]
-            end
+    for x in eachrow(grid)
+        for y in eachcol(grid)
+            liveneighbours = countneighbours((x, y), grid)  # Find live neighbours
         end
+        println(x)
     end
     newgrid = grid
     for r in eachrow(newgrid)
