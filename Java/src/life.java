c89777ece06140c4271b3faf224c3f823cee5e75
Paul <paulbyrne99@rocketmail.com>
2020-06-15 22:45:38 +0100

    skeletons added

--- Java/src/life.java
+++ Java/src/life.java
@@ -4,27 +4,43 @@ public class life {
 		runLife(blinker, 3);
 	}
 	
+	// Run the game
 	public static void runLife(int[][] grid, int steps) {
 		while(steps > 0) {
+			int[][] newgrid = grid;
+			for(int i = 0; i<grid.length; i++) {
+				for(int j = 0; j<grid[1].length; j++) {
+					if (neighbours(i, j, grid) == 0) {
+						
+					}
+				}
+			}
 			display(grid);
 			steps--;
 		}
 		
 	}
 	
+	// Display a grid
 	static void display(int[][] grid) {
 		for(int i = 0; i<grid.length; i++) {
 			for(int j = 0; j<grid[1].length; j++) {
 				if(grid[i][j] == 1) {
-					System.out.print('#');
+					System.out.print("# ");
 				}
 				else { 
-					System.out.print('.');
+					System.out.print(". ");
 				}
 			}
 			System.out.print('\n');
 		}
 		System.out.print('\n');
+	}
+	
+	// Finds the number of neighbours of a cell
+	static int neighbours(int x, int y, int[][] grid) {
+		int neighbours = 0;
+		return neighbours;
 	}
 
 	// Example patterns 
